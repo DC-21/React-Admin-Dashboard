cff5a4ce4e2ec3da7ac9594e7a9a42bcba23be7c
DC-21 <cholahkuboko@gmail.com>
2023-07-14 12:56:03 +0200

    first commit

--- src/Slide/Logs.jsx
+++ src/Slide/Logs.jsx
@@ -6,26 +6,26 @@ const Logs = () => {
       <div className="row1">
         <p>Admin Name</p>
         <p>Logging time</p>
-        <p>Activity</p>
         <p>Log Out time</p>
+        <p>Activity</p>
       </div>
       <div className="row2">
         <p>Chola Kuboko</p>
         <p>12:18AM 14-07-23</p>
-        <p>Updated User roles for new admins</p>
         <p>4:20PM 14-07-23</p>
+        <p>Updated User roles for new admins</p>
       </div>
       <div className="row2">
         <p>Mwape Emmanuel</p>
         <p>10:00AM 13-07-23</p>
-        <p>Updated records in transactions table</p>
         <p>2:10PM 13-07-23</p>
+        <p>Updated records in transactions table</p>
       </div>
       <div className="row2">
         <p>Tamani Phiri</p>
         <p>12:01AM 14-07-23</p>
-        <p>Approved all pending user requests</p>
         <p>2:10PM 14-07-23</p>
+        <p>Approved all pending user requests</p>
       </div>
     </div>
   );
