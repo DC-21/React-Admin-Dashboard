de177e2874f21ea518a02345f491a422a123493f
DC-21 <cholahkuboko@gmail.com>
2023-07-14 14:58:25 +0200

    first commit

--- src/Slide/Logs.jsx
+++ src/Slide/Logs.jsx
@@ -6,56 +6,56 @@ const Logs = () => {
       <div className="row1">
         <p>Admin Name</p>
         <p>Logging time</p>
-        <p>Log Out time</p>
         <p>Activity</p>
+        <p>Log Out time</p>
       </div>
       <div className="row2">
         <p>Chola Kuboko</p>
         <p>12:18AM 14-07-23</p>
-        <p>4:20PM 14-07-23</p>
         <p>Updated User roles for new admins</p>
+        <p>4:20PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Mwape Emmanuel</p>
         <p>10:00AM 13-07-23</p>
-        <p>2:10PM 13-07-23</p>
         <p>Updated records in transactions table</p>
+        <p>4:20PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Tamani Phiri</p>
         <p>12:01AM 14-07-23</p>
-        <p>2:10PM 14-07-23</p>
         <p>Approved all pending user requests</p>
+        <p>2:10PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Rabbi Kindalo</p>
         <p>12:01AM 14-07-23</p>
-        <p>2:10PM 14-07-23</p>
         <p>Created new product listings</p>
+        <p>2:10PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Tamani Phiri</p>
         <p>12:01AM 14-07-23</p>
-        <p>2:10PM 14-07-23</p>
         <p>Approved all pending user requests</p>
+        <p>2:10PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Rabbi Kindalo</p>
         <p>12:01AM 14-07-23</p>
-        <p>2:10PM 14-07-23</p>
         <p>Created new product listings</p>
+        <p>2:10PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Tamani Phiri</p>
         <p>12:01AM 14-07-23</p>
-        <p>2:10PM 14-07-23</p>
         <p>Approved all pending user requests</p>
+        <p>2:10PM 14-07-23</p>
       </div>
       <div className="row2">
         <p>Rabbi Kindalo</p>
         <p>12:01AM 14-07-23</p>
-        <p>2:10PM 14-07-23</p>
         <p>Created new product listings</p>
+        <p>2:10PM 14-07-23</p>
       </div>
     </div>
   );
