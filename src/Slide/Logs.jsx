88115c0dff2fcb0e7608fd734fa4eede9e0d979a
DC-21 <cholahkuboko@gmail.com>
2023-07-14 12:40:36 +0200

    first commit

--- src/Slide/Logs.jsx
+++ src/Slide/Logs.jsx
@@ -1,34 +1,34 @@
-import './log.css'
+import "./log.css";
 
 const Logs = () => {
   return (
-    <div className='logs'>
-        <div className='row1'>
-            <p>Admin Name</p>
-            <p>Logging time</p>
-            <p>Activity</p>
-            <p>Log Out time</p>
-        </div>
-        <div className='row2'>
+    <div className="logs">
+      <div className="row1">
         <p>Admin Name</p>
-            <p>Logging time</p>
-            <p>Activity</p>
-            <p>Log Out time</p>
-        </div>
-        <div className='row3'>
-        <p>Admin Name</p>
-            <p>Logging time</p>
-            <p>Activity</p>
-            <p>Log Out time</p>
-        </div>
-        <div className='row4'>
-        <p>Admin Name</p>
-            <p>Logging time</p>
-            <p>Activity</p>
-            <p>Log Out time</p>
-        </div>
+        <p>Logging time</p>
+        <p>Activity</p>
+        <p>Log Out time</p>
+      </div>
+      <div className="row2">
+        <p>Chola Kuboko</p>
+        <p>12:18AM 14-07-23</p>
+        <p>Updated User roles for new admins</p>
+        <p>4:20PM 14-07-23</p>
+      </div>
+      <div className="row2">
+        <p>Mwape Emmanuel</p>
+        <p>10:00AM 13-07-23</p>
+        <p>Updated records in transactions table</p>
+        <p>2:10PM 13-07-23</p>
+      </div>
+      <div className="row2">
+        <p>Tamani Phiri</p>
+        <p>12:01AM 14-07-23</p>
+        <p>Approved all pending user requests</p>
+        <p>2:10PM 14-07-23</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Logs
+export default Logs;
