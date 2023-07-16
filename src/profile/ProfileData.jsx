d5f8e5906cc9fe411b0c206f92426f170451b392
DC-21 <cholahkuboko@gmail.com>
2023-07-16 17:35:05 +0200

    first commit

--- src/profile/ProfileData.jsx
+++ src/profile/ProfileData.jsx
@@ -1,10 +1,16 @@
-const profileData = [
-  {
-    id: 1,
-    name: "John Doe",
-    bio: "Software Developer",
-    profilePic: "/images/profile1.jpg",
-  },
-];
+import React from 'react'
+import admin from './admin.jpg';
 
-export default profileData;
+const ProfileData = () => {
+  return (
+    <div>
+      <div>
+        <img src={admin}/>
+        <h1>Chola Kuboko</h1>
+        <p>Admin</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileData
