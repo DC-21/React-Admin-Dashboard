7a9f7978de5d4aef01066cd463eef56449af3fb4
DC-21 <cholahkuboko@gmail.com>
2023-07-16 11:27:53 +0200

    first commit

--- src/profile/Pro.jsx
+++ src/profile/Pro.jsx
@@ -19,7 +19,14 @@ const Pro = ({ profileData }) => {
       onMouseLeave={handleMouseLeave}
     >
       <div className="profile-icon">
-        <i className="fas fa-user"></i>
+      <svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      height="2em"
+      width="2em"
+    >
+      <path d="M399 384.2c-22.1-38.4-63.6-64.2-111-64.2h-64c-47.4 0-88.9 25.8-111 64.2 35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM512 256c0 141.4-114.6 256-256 256S0 397.4 0 256 114.6 0 256 0s256 114.6 256 256zm-256 16c39.8 0 72-32.2 72-72s-32.2-72-72-72-72 32.2-72 72 32.2 72 72 72z" />
+    </svg>
       </div>
       {isHovered && (
         <div className="profile-popup">
