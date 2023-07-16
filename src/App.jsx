833e95b070436bcbcee4611406173e0cf4985cd8
DC-21 <cholahkuboko@gmail.com>
2023-07-16 11:38:16 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Pro from './profile/Pro';
-import './App.css';
-
 const App = () => {
   const profileData = {
     name: 'John Doe',
@@ -10,7 +8,6 @@ const App = () => {
 
   return (
     <div className='main'>
-      <h1>My App</h1>
       <Pro profileData={profileData} />
     </div>
   );
