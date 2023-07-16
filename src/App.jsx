3be56e840c4b142d54165f79c04cc11b090343aa
DC-21 <cholahkuboko@gmail.com>
2023-07-16 13:04:11 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Pro from './profile/Pro';
 import ProfileData from './profile/ProfileData';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <div className="main">
-        <Route path="/" element={<Pro profileData={ProfileData} />} />
+        <Pro profileData={ProfileData} />
         <Routes>
           <Route path="/profile/:id" element={<ProfileData />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 };
 
