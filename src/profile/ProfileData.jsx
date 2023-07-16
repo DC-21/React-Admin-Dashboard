1e7e6f187452832a437c34b1558b176d4ee46253
DC-21 <cholahkuboko@gmail.com>
2023-07-16 18:23:20 +0200

    first commit

--- src/profile/ProfileData.jsx
+++ src/profile/ProfileData.jsx
@@ -7,11 +7,22 @@ const ProfileData = () => {
     <div className="container">
       <div className="image">
         <img src={admin} />
-        <h1>Chola Kuboko</h1>
-        <p>Admin</p>
-        <p>cholahkuboko@gmail.com</p>
-        <p>0979054417</p>
-        <p>9 miles, Lusaka</p>
+        <div className="name">
+          <p>Name</p>
+          <p>Chola Kuboko</p>
+        </div>
+        <p>
+          <span></span>Admin
+        </p>
+        <p>
+          <span></span>cholahkuboko@gmail.com
+        </p>
+        <p>
+          <span></span>0979054417
+        </p>
+        <p>
+          <span></span>9 miles, Lusaka
+        </p>
       </div>
     </div>
   );
