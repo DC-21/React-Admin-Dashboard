b76443bdb026059119aba89edb122f9170a30bdb
DC-21 <cholahkuboko@gmail.com>
2023-07-16 18:15:10 +0200

    first commit

--- src/profile/ProfileData.jsx
+++ src/profile/ProfileData.jsx
@@ -5,7 +5,7 @@ import admin from "./admin.jpg";
 const ProfileData = () => {
   return (
     <div className="container">
-      <div className="centered-content">
+      <div className="image">
         <img src={admin} />
         <h1>Chola Kuboko</h1>
         <p>Admin</p>
